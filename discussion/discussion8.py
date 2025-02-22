bb5f6d617d3f673af3a68c454ef04a8f5cac8c1d
YHL <1241171248@qq.com>
2025-02-22 13:18:53 +0800

    lab07, discussion8

---
+++ discussion/discussion8.py
@@ -0,0 +1,147 @@
+def strange_loop():
+    """Return a Link s for which s.rest.first.rest is s.
+
+    >>> s = strange_loop()
+    >>> s.rest.first.rest is s
+    True
+    """
+    s = Link(1, Link(Link(2)))
+    s.rest.first.rest = s
+    return s
+
+
+def sum_rec(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_rec(a, 2)
+    7
+    >>> sum_rec(a, 5)
+    15
+    >>> sum_rec(Link.empty, 1)
+    0
+    """
+    # Use a recursive call to sum_rec; don't call sum_iter
+    if s is Link.empty: return 0
+    elif k == 1: return s.first
+    return s.first + sum_rec(s.rest, k - 1)
+
+def sum_iter(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_iter(a, 2)
+    7
+    >>> sum_iter(a, 5)
+    15
+    >>> sum_iter(Link.empty, 1)
+    0
+    """
+    # Don't call sum_rec or sum_iter
+    res = 0
+    while k > 0 and s is not Link.empty:
+        res += s.first
+        s, k = s.rest, k - 1
+    return res
+
+
+def overlap(s, t):
+    """For increasing s and t, count the numbers that appear in both.
+
+    >>> a = Link(3, Link(4, Link(6, Link(7, Link(9, Link(10))))))
+    >>> b = Link(1, Link(3, Link(5, Link(7, Link(8)))))
+    >>> overlap(a, b)  # 3 and 7
+    2
+    >>> overlap(a.rest, b)  # just 7
+    1
+    >>> overlap(Link(0, a), Link(0, b))
+    3
+    """
+    res = 0
+    while s is not Link.empty and t is not Link.empty:
+        if s.first < t.first: s = s.rest
+        elif s.first > t.first: t = t.rest
+        else: res, s, t = res + 1, s.rest, t.rest
+    return res
+
+def divide(n, d):
+    """Return a linked list with a cycle containing the digits of n/d.
+
+    >>> display(divide(5, 6))
+    0.8333333333...
+    >>> display(divide(2, 7))
+    0.2857142857...
+    >>> display(divide(1, 2500))
+    0.0004000000...
+    >>> display(divide(3, 11))
+    0.2727272727...
+    >>> display(divide(3, 99))
+    0.0303030303...
+    >>> display(divide(2, 31), 50)
+    0.06451612903225806451612903225806451612903225806451...
+    
+    """
+    assert n > 0 and n < d
+    result = Link(0)  # The zero before the decimal point
+    cache, tmp, n = {}, result, n * 10
+    while True:
+        cur, n = n // d, n % d * 10
+        tmp.rest = Link(cur)
+        tmp = tmp.rest
+        if n in cache: break
+        cache[n] = tmp
+    tmp.rest = cache[n].rest
+    return result
+
+
+def display(s, k=10):
+    """Print the first k digits of infinite linked list s as a decimal.
+
+    >>> s = Link(0, Link(8, Link(3)))
+    >>> s.rest.rest.rest = s.rest.rest
+    >>> display(s)
+    0.8333333333...
+    """
+    assert s.first == 0, f'{s.first} is not 0'
+    digits = f'{s.first}.'
+    s = s.rest
+    for _ in range(k):
+        assert s.first >= 0 and s.first < 10, f'{s.first} is not a digit'
+        digits += str(s.first)
+        s = s.rest
+    print(digits + '...')
+
+
+class Link:
+    """A linked list is either a Link object or Link.empty
+
+    >>> s = Link(3, Link(4, Link(5)))
+    >>> s.rest
+    Link(4, Link(5))
+    >>> s.rest.rest.rest is Link.empty
+    True
+    >>> s.rest.first * 2
+    8
+    >>> print(s)
+    <3 4 5>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
